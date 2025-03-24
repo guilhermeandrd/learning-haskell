ef813bf7cd292ccf3e01a48273e0f87f1889cccb
guilhermeandrd <nickyandrade331@gmail.com>
2025-03-24 16:32:47 -0300

    aprendendo haskell na forca do odio

---
+++ teste.hs
@@ -0,0 +1,17 @@
+dobro2 x y = 2 * x + 2 * y
+triplo x = 3 * x
+dobro3 x y =
+    dobro x + dobro y
+dobro x = 2 * x
+
+condicao x = if x>10
+    then x
+    else x*5
+
+letra = ['a', 'b', 'c', 'd', 'e']
+numeros = [1, 2, 3, 4, 5]
+letra2 = ['f', 'g']
+numero2 = [6, 7, 8, 9]
+vazio = []
+vazio2 = [vazio]
+vazio3 = [vazio2]

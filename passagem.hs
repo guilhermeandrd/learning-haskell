c471b5e2193cc8202add4fc91603d64ad42574ef
guilhermeandrd <nickyandrade331@gmail.com>
2025-04-01 16:48:11 -0300

    função de passagem
    retorna um desconto de acordo com a idade do passageiro

---
+++ passagem.hs
@@ -0,0 +1,8 @@
+passagem :: Double -> Double -> Double
+
+passagem a b | b > 59 = a * 0.6
+             | b < 11 && b > 2 = a * 0.5
+             | b <= 2 = a * 0.1
+             | otherwise = a
+
+-- otherwise para representar outros casos

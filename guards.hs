49266ca6c1e1e7a3545380cdd986631e63838af0
guilhermeandrd <nickyandrade331@gmail.com>
2025-05-13 09:58:48 -0300

    aula sobre guards

--- guards.hs
+++ guards.hs
@@ -11,3 +11,33 @@ imc p h
 -- se é acutangulo
 -- se existe
 -- se é
+
+tri :: int -> Int -> Int -> String
+
+tri a b c | existe && maior^2 == bg^2 + cg^2  = "existe e eh retangulo"
+          | existe && (maior^2 < bg^2 + cg^2)  = "existe e eh acutangulo"
+          | existe && (maior^2 > bg^2 + cg^2) = "existe e eh obtusangulo"
+          | otherwise = "nao existe"
+    where
+        existe = a < b+c && a > negate b-c || b < a+c && b > negate a-c || c < a+b && c > negate a-b
+        ls = a : b : [c]
+        maior = maximum ls
+        bg = ((sum ls) - maior) `div` 2
+        cg = sum ls - maior - bg
+
+
+diff a b c
+    | a > b && a > c = a*a-b*b-c*c
+    | b > c = b * b - a * a - c* c
+    | otherwise = c*c-a*a-b*b
+     
+existe a b c = a<b+c && a > abs (b-c)
+ 
+triang a b c
+    | e && (d==0) = "existe e eh retangulo"
+    | e && (d<0)  = "existe e eh acutangulo"
+    | e && (d>0)  = "existe e eh obstusangulo"
+    | otherwise = "o triangulo nao existe"
+    where
+        e = existe a b c
+        d = diff a b c
